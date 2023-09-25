9fec2b55adc2e72c7363733993ec7e5e0e8c1e9e
nicolas minieri <nicolasminieri141@gmail.com>
2023-09-24 22:52:23 -0300

    cds

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const mineflayer = require("mineflayer");
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const {Server} = require("socket.io");
+const io = new Server(server);
+
+app.get("/",(req, res)=>{
+    res.send("servidor")
+});
+
 
 const Bot = mineflayer.createBot({
     host:"CONURBA.aternos.me",
